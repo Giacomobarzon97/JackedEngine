b243f0be96dc56116caa681652fe22927c5960c2
Unknown <gtm459@gmail.com>
2023-01-14 23:11:01 +0100

    Created Base Shaders

--- JackedEngine/Main.cpp
+++ JackedEngine/Main.cpp
@@ -1,7 +1,10 @@
 #include "Window.h"
+#include "Pipeline.h"
 
 int main() {
 	Window window{ 800, 800, "Hello Vulkan!" };
+	Pipeline pipeline{"Shaders/default.vert.spv", "Shaders/default.frag.spv"};
+
 	while (!window.shouldClose()) {
 		glfwPollEvents();
 	}
